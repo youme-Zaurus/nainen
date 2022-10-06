1580c3a0de04f73a8ab522a16bcb53abc68db3ef
Yumi <k019c1409@g.neec.ac.jp>
2022-10-06 10:58:39 +0900

    *[update]settings_local_sample.pyにDB情報を追加

--- settings_local_sample.py
+++ settings_local_sample.py
@@ -2,10 +2,16 @@
 SECRET_KEY = ''
 
 # DB接続情報
-DATABASES = {
-    'default': {
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'root',
+#         'USER': 'nainen',
+#         'PASSWORD': 'Administr@t0r',
+#         # 'HOST': '127.0.0.1',
+#         # 'PORT': '5432',
+#     }
+# }
 
 # XXのキー
-XXX = ''
+# XX = ''
