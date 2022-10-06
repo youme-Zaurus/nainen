a9448a5fb627fd64340ecba83dc307af28420731
Yumi <k019c1409@g.neec.ac.jp>
2022-10-06 11:09:46 +0900

    [update]DB接続情報のコメントアウトを消す

--- settings_local_sample.py
+++ settings_local_sample.py
@@ -2,16 +2,16 @@
 SECRET_KEY = ''
 
 # DB接続情報
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'nainen',
-#         'USER': 'root',
-#         'PASSWORD': 'Administr@t0r',
-#         # 'HOST': '127.0.0.1',
-#         # 'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nainen',
+        'USER': 'root',
+        'PASSWORD': 'Administr@t0r',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
+    }
+}
 
 # XXのキー
 # XX = ''
