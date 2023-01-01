0a3e270a0106183971e9457adbf6b4b5bef58546
Yumi <k019c1409@g.neec.ac.jp>
2023-01-02 05:15:23 +0900

    [update]許可するホストの変更 とっこちゃんの設定を一時的に変更

--- django/config/settings.py
+++ django/config/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.68.242.173']
+ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['52.68.242.173']
 
 
 # Application definition
